0a945ad7c8b473dc80fee15e2d9f639675de725c
Dennis O'Brien <dco6453@me.com>
2024-10-07 17:38:50 -0700

    Added Joseph's latest work

--- MeepMeep7462/src/main/java/com/example/meepmeep7462/MeepMeep7462.java
+++ MeepMeep7462/src/main/java/com/example/meepmeep7462/MeepMeep7462.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep7462;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -10,19 +11,28 @@ public class MeepMeep7462 {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .build());
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 14)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-31.00, 62.5, Math.toRadians(-90.00)))
+                        .splineToConstantHeading(new Vector2d(-31.0, 30.00), Math.toRadians(-90.0))
+                        .splineToConstantHeading(new Vector2d(-35.0, 20.00), Math.toRadians(-130.0))
+                        .splineToConstantHeading(new Vector2d(-40.0, 16.0), Math.toRadians(180.0))
+                        .splineToConstantHeading(new Vector2d(-45.0, 20.0), Math.toRadians(110.0))
+                        .splineToConstantHeading(new Vector2d(-47.0, 57.91), Math.toRadians(90))
+                        .waitSeconds(0.5)
+                        .splineToConstantHeading(new Vector2d(-50, 20.0), Math.toRadians(-135.0))
+                        .splineToConstantHeading(new Vector2d(-52.0, 19.0), Math.toRadians(180.0))
+                        .splineToConstantHeading(new Vector2d(-54.0, 20.0), Math.toRadians(135.0))
+                        .splineToConstantHeading(new Vector2d(-57.0, 57.91), Math.toRadians(90.0))
+                        .waitSeconds(0.5)
+                        .splineToConstantHeading(new Vector2d(-57, 20.0), Math.toRadians(-90.0))
+                        .splineToConstantHeading(new Vector2d(-63.0, 20.0), Math.toRadians(90.0))
+                        .splineToConstantHeading(new Vector2d(-63.0, 57.91), Math.toRadians(90.0))
+                        .build()
+                );
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -1,28 +1,82 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
+import com.acmerobotics.roadrunner.AccelConstraint;
+import com.acmerobotics.roadrunner.AngularVelConstraint;
+import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
 
+import java.util.Arrays;
+
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(20.88, 62.63, Math.toRadians(269.42));
+        Pose2d beginPose = new Pose2d(-31.00, 62.5, Math.toRadians(-90.00));
+//        VelConstraint baseVelConstraint = new MinVelConstraint(Arrays.asList(
+//                new TranslationalVelConstraint(100.0),
+//                new AngularVelConstraint(Math.PI / 2)
+//        ));
+//        AccelConstraint baseAccelConstraint = new ProfileAccelConstraint(-30.0, 100.0);
+        //hardwareMap.get(WebcamName.class, "Webcam 1")
+        VelConstraint baseVelConstraint = new MinVelConstraint(Arrays.asList(
+                new TranslationalVelConstraint(40.0),
+                new AngularVelConstraint(Math.PI / 2)
+        ));
+        VelConstraint curveVelConstraint = new MinVelConstraint(Arrays.asList(
+                new TranslationalVelConstraint(15.0),
+                new AngularVelConstraint(Math.PI / 2)
+        ));
+        AccelConstraint baseAccelConstraint = new ProfileAccelConstraint(-20.0, 50.0);
+
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+
 
 
             waitForStart();
 //          57.74, 55.91), Math.toRadians(44.44)
             Actions.runBlocking(
                 drive.actionBuilder(beginPose)
-                        .splineTo(new Vector2d(57.74, 55.91), Math.toRadians(44.44))
-//                        .splineTo(new Vector2d(0, 30), Math.PI)
-                        .build());
+//                        .lineToY(-30,baseVelConstraint,baseAccelConstraint)
+//                        .build()
+//                        .splineToConstantHeading(new Vector2d(-33.0, 30.00), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
+//                        .splineToConstantHeading(new Vector2d(-33.0, 11.0), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
+//                        .strafeToConstantHeading(new Vector2d(-47.0, 11.0), baseVelConstraint,baseAccelConstraint)
+//                        .strafeToConstantHeading(new Vector2d(-47.0, 57.91), baseVelConstraint,baseAccelConstraint)
+//                        .waitSeconds(0.5)
+//                        .splineToConstantHeading(new Vector2d(-47, 11.0), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
+//                        .strafeToConstantHeading(new Vector2d(-57.0, 11.0), baseVelConstraint,baseAccelConstraint)
+//                        .strafeToConstantHeading(new Vector2d(-57.0, 57.91), baseVelConstraint,baseAccelConstraint)
+//                        .waitSeconds(0.5)
+//                        .strafeToConstantHeading(new Vector2d(-57, 11.0), baseVelConstraint,baseAccelConstraint)
+//                        .strafeToConstantHeading(new Vector2d(-64.0, 11.0), baseVelConstraint,baseAccelConstraint)
+//                        .strafeToConstantHeading(new Vector2d(-64.0, 57.91), baseVelConstraint,baseAccelConstraint)
+//                        .build()
+                        .splineToConstantHeading(new Vector2d(-31.0, 30.00), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-35.0, 14.00), Math.toRadians(-130.0), baseVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-40.0, 10.0), Math.toRadians(180.0), curveVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-45.0, 14.0), Math.toRadians(110.0), curveVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-47.0, 57.91), Math.toRadians(90), baseVelConstraint,baseAccelConstraint)
+                        .waitSeconds(0.5)
+                        .splineToConstantHeading(new Vector2d(-50, 14.0), Math.toRadians(-135.0), baseVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-52.0, 13.0), Math.toRadians(180.0), curveVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-54.0, 14.0), Math.toRadians(135.0), curveVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-57.0, 57.91), Math.toRadians(90.0), baseVelConstraint,baseAccelConstraint)
+                        .waitSeconds(0.5)
+                        .splineToConstantHeading(new Vector2d(-57, 12.0), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
+                        .splineToConstantHeading(new Vector2d(-63.0, 12.0), Math.toRadians(-180.0), baseVelConstraint,baseAccelConstraint)
+                        .strafeToConstantHeading(new Vector2d(-63.0, 57.91), baseVelConstraint,baseAccelConstraint)
+                        .build()
+                );
         } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
             TankDrive drive = new TankDrive(hardwareMap, beginPose);
 
