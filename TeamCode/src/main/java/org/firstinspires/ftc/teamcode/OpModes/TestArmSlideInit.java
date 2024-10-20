cd0079674e6d0e07313511b4b6dd5e82342d6623
Dennis O'Brien <dco6453@me.com>
2024-10-20 08:34:01 -0700

    Latest work by Joseph & Alex prior to our 1st scrimage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestArmSlideInit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestArmSlideInit.java
@@ -10,7 +10,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
-public class FieldCentricMecanumTeleOp extends LinearOpMode {
+public class TestArmSlideInit extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Put constants here
@@ -60,19 +60,15 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
 
 
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        arm.setPower(1.0);
+        arm.setPower(0.0);
 //        if(reverse) {arm.setDirection(DcMotorSimple.Direction.REVERSE);}
-        arm.setTargetPosition(0);
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        tickLimit = maxVal;
 
         slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        slide.setPower(1.0);
+        slide.setPower(0.0);
 //        if(reverse) {arm.setDirection(DcMotorSimple.Direction.REVERSE);}
-        slide.setTargetPosition(0);
-        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        tickLimit = maxVal;
 
         // Retrieve the IMU from the hardware map
@@ -89,66 +85,18 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = -gamepad1.left_stick_x;
-            double rx = -gamepad1.right_stick_x;
-
-
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
-                imu.resetYaw();
+            if(gamepad2.a){
+                arm.setPower(-0.3);
+            } else if(gamepad2.y){
+                arm.setPower(-0.5);
+            }  else {
+                arm.setPower(0.0);
             }
-
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-            if (gamepad2.left_stick_y != 0){
-                armTargetPosition += Math.floor(-gamepad2.left_stick_y*armRotationSpeed);
-                arm.setTargetPosition(armTargetPosition);
-                armTargetPosition = Math.min(Math.max(armMinPosition,armTargetPosition),armMaxPosition);
+            if(gamepad2.b){
+                slide.setPower(-0.03);
+            } else {
+                slide.setPower(0.0);
             }
-            if (gamepad2.right_stick_y != 0){
-                slideTargetPosition += Math.floor(-gamepad2.right_stick_y*slideMovementSpeed);
-                slideTargetPosition = Math.min(Math.max(slideMinPosition,slideTargetPosition),slideMaxPosition);
-                slide.setTargetPosition(slideTargetPosition);
-            }
-            if (gamepad2.dpad_up){
-                wristTargetPosition += wristSpeed;
-                wristTargetPosition = Math.min (wristMax,wristTargetPosition);
-            } else if (gamepad2.dpad_down){
-                wristTargetPosition -= wristSpeed;
-                wristTargetPosition = Math.max (wristMin,wristTargetPosition);
-            }
-            wrist.setPosition(wristTargetPosition);
-            if (gamepad2.right_bumper){
-                clawTargetPosition += clawSpeed;
-                clawTargetPosition = Math.min (clawMax,clawTargetPosition);
-            } else if (gamepad2.left_bumper){
-                clawTargetPosition -= clawSpeed;
-                clawTargetPosition = Math.max (clawMin,clawTargetPosition);
-            }
-            claw.setPosition(clawTargetPosition);
 
             telemetry.addData("armPosition", arm.getCurrentPosition());
             telemetry.addData("slidePosition", slide.getCurrentPosition());
