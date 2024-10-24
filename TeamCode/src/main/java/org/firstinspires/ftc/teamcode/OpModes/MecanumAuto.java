d779b414df141aa272dc11d2bbc54a55c8f44c8d
Dennis O'Brien <dco6453@me.com>
2024-10-24 15:01:07 -0700

    Got 'MecanumAuto' to move the robbot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MecanumAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MecanumAuto.java
@@ -1,35 +1,37 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import static org.firstinspires.ftc.vision.VisionPortal.CameraState.OPENING_CAMERA_DEVICE;
-
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.AccelConstraint;
+import com.acmerobotics.roadrunner.AngularVelConstraint;
+import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
 
-import org.firstinspires.ftc.teamcode.NewMecanumDrive;
-import org.firstinspires.ftc.teamcode.gamepad.InputAutoMapper;
-import org.firstinspires.ftc.teamcode.gamepad.InputHandler;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Pose2dWrapper;
 import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
 import org.firstinspires.ftc.teamcode.ComputerVision;
-import com.acmerobotics.roadrunner.Trajectory;
+
+import java.util.Arrays;
 
 @Config
 @Autonomous(name = "MecanumAuto")
-public class MecanumAuto extends LinearOpMode {
-    public static double INITIAL_X = 0;
+public final class MecanumAuto extends LinearOpMode {
+    public static double INITIAL_X = -6;
     public static double INITIAL_Y = 55;
 
     public static double ROTATIONINITIAL = 0;
 
-    public static double PLACEONE_X = 0;
-    public static double PLACEONE_Y = 43;
+    public static double PLACEONE_X = -36;
+    public static double PLACEONE_Y = 50;
 
     public static double PLACETWO_X = -40;
     public static double PLACETWO_Y = 43;
@@ -52,7 +54,6 @@ public class MecanumAuto extends LinearOpMode {
     public static double PLACESEVEN_X = 0;
     public static double PLACESEVEN_Y = 43;
 
-
     ComputerVision vision;
     AprilTagPoseFtc[] aprilTagTranslations = new AprilTagPoseFtc[11];
     //InputHandler inputHandler;
@@ -65,19 +66,10 @@ public class MecanumAuto extends LinearOpMode {
     int startDelay = 0;
     int i = 1; //used as an iterator for outputLog()
 
-    public Pose2dWrapper startPose = new Pose2dWrapper(INITIAL_X, INITIAL_Y, Math.toRadians(90));
-    public Pose2dWrapper poseOne = new Pose2dWrapper(PLACEONE_X, PLACEONE_Y, Math.toRadians(90));
-    public Pose2dWrapper poseTwo = new Pose2dWrapper(PLACETWO_X, PLACETWO_Y, Math.toRadians(90));
-    public Pose2dWrapper poseThree = new Pose2dWrapper(PLACETHREE_X, PLACETHREE_Y, Math.toRadians(90));
-    public Pose2dWrapper poseFour = new Pose2dWrapper(PLACEFOUR_X, PLACEFOUR_Y, Math.toRadians(90));
-    public Pose2dWrapper poseFive = new Pose2dWrapper(PLACEFIVE_X, PLACEFIVE_Y, Math.toRadians(90));
-    public Pose2dWrapper poseSix = new Pose2dWrapper(PLACESIX_X, PLACESIX_Y, Math.toRadians(90));
-    public Pose2dWrapper poseSeven = new Pose2dWrapper(PLACESEVEN_X, PLACESEVEN_Y, Math.toRadians(90));
-
     @Override
     public void runOpMode() throws InterruptedException {
 
-        runtime.reset();
+        //runtime.reset();
         //inputHandler = InputAutoMapper.normal.autoMap(this);
         /*while (inputComplete == false) {
 
@@ -100,13 +92,39 @@ public class MecanumAuto extends LinearOpMode {
         while (vision.visionPortal.getCameraState() == OPENING_CAMERA_DEVICE) {}
 
         vision.setActiveCameraOne(); */
-        NewMecanumDrive drive = new NewMecanumDrive(hardwareMap, startPose.toPose2d());
+        Pose2dWrapper startPose = new Pose2dWrapper(INITIAL_X, INITIAL_Y, Math.toRadians(-90));
+        Pose2dWrapper poseOne = new Pose2dWrapper(PLACEONE_X, PLACEONE_Y, Math.toRadians(-90));
+        Pose2dWrapper poseTwo = new Pose2dWrapper(PLACETWO_X, PLACETWO_Y, Math.toRadians(90));
+        Pose2dWrapper poseThree = new Pose2dWrapper(PLACETHREE_X, PLACETHREE_Y, Math.toRadians(90));
+        Pose2dWrapper poseFour = new Pose2dWrapper(PLACEFOUR_X, PLACEFOUR_Y, Math.toRadians(90));
+        Pose2dWrapper poseFive = new Pose2dWrapper(PLACEFIVE_X, PLACEFIVE_Y, Math.toRadians(90));
+        Pose2dWrapper poseSix = new Pose2dWrapper(PLACESIX_X, PLACESIX_Y, Math.toRadians(90));
+        Pose2dWrapper poseSeven = new Pose2dWrapper(PLACESEVEN_X, PLACESEVEN_Y, Math.toRadians(90));
+
+        VelConstraint baseVelConstraint = new MinVelConstraint(Arrays.asList(
+                new TranslationalVelConstraint(40.0),
+                new AngularVelConstraint(Math.PI / 2)
+        ));
+        VelConstraint curveVelConstraint = new MinVelConstraint(Arrays.asList(
+                new TranslationalVelConstraint(15.0),
+                new AngularVelConstraint(Math.PI / 2)
+        ));
+        AccelConstraint baseAccelConstraint = new ProfileAccelConstraint(-20.0, 50.0);
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPose.toPose2d());
+
+        if (opModeInInit()) {
+            telemetry.addData("Software was compiled on:", "aaa");
+            telemetry.update();
+        }
 
         waitForStart();
+//                        .splineToConstantHeading(new Vector2d(-33.0, 30.00), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
 
         Actions.runBlocking(
                 drive.actionBuilder(startPose.toPose2d())
-                        .strafeTo(poseOne.toPose2d().position)
+                        .splineToConstantHeading(new Vector2d(PLACEONE_X, PLACEONE_Y), Math.toRadians(-90.0), baseVelConstraint,baseAccelConstraint)
+                        //.strafeTo(poseOne.toPose2d().position)
                         //.strafeTo(poseTwo.toPose2d().position)
                         //.strafeTo(poseThree.toPose2d().position)
                         //.strafeTo(poseFour.toPose2d().position)
@@ -120,7 +138,7 @@ public class MecanumAuto extends LinearOpMode {
         outputLog(drive); //1
 
     }
-    public void outputLog (NewMecanumDrive drive){
+    public void outputLog (MecanumDrive drive){
         RobotLog.d("WAY: Current Robot Pose Estimate and time: X: %.03f Y: %.03f Heading: %.03f ms: %.03f iteration: %d", drive.pose.position.x, drive.pose.position.y, Math.toDegrees(drive.pose.heading.real), runtime.milliseconds(), i);
         i++;
     }
